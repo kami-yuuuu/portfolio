35f8aff725b76df285e36d50192b749eb7492254
kami-yuuuu <92091721+kami-yuuuu@users.noreply.github.com>
2024-02-26 13:40:11 +0900

    httpsに対応するように変更

--- app/node/portfolio/src/App.vue
+++
@@ -1,8 +0,0 @@
-<script setup lang="ts">
-import titanic from './components/titanic.vue'
-</script>
-
-<template>
-<titanic></titanic>
-</template>
-
--- app/node/portfolio/tailwind.config.js
+++ app/node/portfolio/tailwind.config.js
@@ -1,9 +1,11 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
-  plugins: [require('daisyui')],
+  variants: {
+    extend: {},
+  },
+  plugins: [],
 }
-
