855959ecfc27ca49b7a8d4d80aad9af2a003cd5b
kami-yuuuuu <craft.boss1697@gmail.com>
2025-10-19 04:37:55 +0900

    スキーマを修正

--- backend/models/category.py
+++ backend/models/category.py
@@ -1,8 +1,10 @@
 from sqlmodel import Field, SQLModel, String, create_engine
-from typing import Optional
+from typing import Optional,List, TYPE_CHECKING
 from sqlmodel import Relationship, func, Column, DateTime
-from .transaction import Transaction
+from datetime import datetime
 
+if TYPE_CHECKING:
+    from .transaction import Transaction
 
 
 class Category(SQLModel, table=True):
@@ -10,6 +12,6 @@ class Category(SQLModel, table=True):
     name:str = Field(description="カテゴリの名前", unique=True)
     type: str = Field(description="取引の分類（収入/支出など）")
     color:str = Field(default="#FFFFFF", description="カテゴリの色を表す16進カラーコード", unique=True)
-    transactions: list[Transaction] = Relationship(back_populates="category")
-    created_at: Optional[str] = Field(default=None, description="作成日時", sa_column=Column(DateTime(timezone=True), server_default=func.now()))
-    updated_at: Optional[str] = Field(default=None, description="更新日時", sa_column=Column(DateTime(timezone=True), onupdate=func.now()))
+    transactions: List['Transaction'] = Relationship(back_populates="category")
+    created_at: Optional[datetime] = Field(default=None, description="作成日時", sa_column=Column(DateTime(timezone=True), server_default=func.now()))
+    updated_at: Optional[datetime] = Field(default=None, description="更新日時", sa_column=Column(DateTime(timezone=True), onupdate=func.now()))
--- backend/models/transaction.py
+++ backend/models/transaction.py
@@ -1,7 +1,8 @@
-from typing import Optional
+from typing import Optional, Dict
 from datetime import date as dates, datetime
-from sqlmodel import SQLModel, Field, Relationship, Column, Numeric
-from sqlalchemy import DateTime, func, JSON 
+from decimal import Decimal
+from sqlmodel import SQLModel, Field, Relationship, Column, JSON
+from sqlalchemy import DateTime, func
 from .category import Category
 from .payment_method import PaymentMethod
 
@@ -12,11 +13,11 @@ class Transaction(SQLModel, table=True):
     date: dates = Field(description="取引日")
     category_id: int = Field(foreign_key="category.id", description="カテゴリID")
     category: Category  = Relationship(back_populates="transactions")
-    amount: Numeric = Field(description="金額", sa_column=Column(Numeric(12, 2)))
+    amount: Decimal = Field(description="取引金額")
     memo: Optional[str] = None
     payment_method_id: int = Field(default=None, foreign_key="paymentmethod.id", description="支払い方法")
     payment_method: Optional["PaymentMethod"] = Relationship(back_populates="transactions")
-    repeat: Optional[JSON] = Field(default=None, description="繰り返し設定", sa_column=Column(JSON))
+    repeat: Dict = Field(description="繰り返し設定", default_factory=dict, sa_column=Column(JSON))
     receipt_url: Optional[str] = Field(default=None, description="レシート画像のURL")
     created_at: Optional[datetime] = Field(default=None, sa_column=Column(DateTime(timezone=True), server_default=func.now()), description="作成日時")
     updated_at: Optional[datetime] = Field(default=None, sa_column=Column(DateTime(timezone=True), onupdate=func.now()), description="更新日時")
