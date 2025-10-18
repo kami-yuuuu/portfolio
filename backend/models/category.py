50de242b9d461690c96f88fdbf0d8e91c65f0b04
kami-yuuuuu <craft.boss1697@gmail.com>
2025-10-19 03:21:35 +0900

    スキーマを修正

--- backend/models/category.py
+++ backend/models/category.py
@@ -1,9 +1,12 @@
 from sqlmodel import Field, SQLModel, String, create_engine
+from typing import Optional
+from sqlmodel import Relationship
+from .transaction import Transaction
 
 
 class Category(SQLModel, table=True):
-    id:int | None = Field(nullable=False, primary_key=True)
-    name:str
+    id:Optional[int] = Field(default=None, primary_key=True)
+    name:str = Field(description="カテゴリの名前", unique=True)
     type: str = Field(description="取引の分類（収入/支出など）")
     color:str = Field(default="#FFFFFF", description="カテゴリの色を表す16進カラーコード", unique=True)
-
+    transactions: list["Transaction"] = Relationship(back_populates="categorys")
--- backend/models/payment_method.py
+++ backend/models/payment_method.py
@@ -4,6 +4,6 @@ from .transaction import Transaction
 
 
 class PaymentMethod(SQLModel, table=True):
-    name: str = Field(primary_key=True, description="支払い方法の名前")
+    name: Optional[str]= Field(default=None, primary_key=True, description="支払い方法の名前")
     description: Optional[str] = Field(default=None, description="支払い方法の説明")
     
