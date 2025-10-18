36f6917a25b92067016dd77f5ff7743c1a10b0d7
kami-yuuuuu <craft.boss1697@gmail.com>
2025-10-19 04:01:56 +0900

    スキーマを修正

--- backend/models/category.py
+++ backend/models/category.py
@@ -1,7 +1,8 @@
 from sqlmodel import Field, SQLModel, String, create_engine
 from typing import Optional
-from sqlmodel import Relationship
+from sqlmodel import Relationship, func, Column, DateTime
 from .transaction import Transaction
+
 
 
 class Category(SQLModel, table=True):
@@ -10,5 +11,5 @@ class Category(SQLModel, table=True):
     type: str = Field(description="取引の分類（収入/支出など）")
     color:str = Field(default="#FFFFFF", description="カテゴリの色を表す16進カラーコード", unique=True)
     transactions: list[Transaction] = Relationship(back_populates="category")
-    created_at: Optional[str] = Field(default=None, description="作成日時")
-    updated_at: Optional[str] = Field(default=None, description="更新日時")
+    created_at: Optional[str] = Field(default=None, description="作成日時", sa_column=Column(DateTime(timezone=True), server_default=func.now()))
+    updated_at: Optional[str] = Field(default=None, description="更新日時", sa_column=Column(DateTime(timezone=True), onupdate=func.now()))
--- backend/models/payment_method.py
+++ backend/models/payment_method.py
@@ -1,4 +1,5 @@
 from sqlmodel import SQLModel, Field, Relationship
+from sqlalchemy import Column, DateTime, func
 from typing import List, Optional
 from .transaction import Transaction
 
@@ -7,7 +8,7 @@ class PaymentMethod(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str = Field(description="支払い方法の名前")
     description: Optional[str] = Field(default=None, description="支払い方法の説明")
-    created_at: Optional[str] = Field(default=None, description="作成日時")
-    updated_at: Optional[str] = Field(default=None, description="更新日時")
+    created_at: Optional[str] = Field(default=None, description="作成日時", sa_column=Column(DateTime(timezone=True), server_default=func.now()))
+    updated_at: Optional[str] = Field(default=None, description="更新日時", sa_column=Column(DateTime(timezone=True), onupdate=func.now()))
 
 
