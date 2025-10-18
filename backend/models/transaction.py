d7b592ca42c5fe4bbc2c7c4e2db5e73d2b0fd074
kami-yuuuuu <craft.boss1697@gmail.com>
2025-10-19 03:51:03 +0900

    transactionのスキーマを修正

--- backend/models/transaction.py
+++ backend/models/transaction.py
@@ -1,7 +1,6 @@
-from typing import Optional, Dict, List 
-from decimal import Decimal
+from typing import Optional
 from datetime import date as dates, datetime
-from sqlmodel import SQLModel, Field, Relationship, Column
+from sqlmodel import SQLModel, Field, Relationship, Column, Numeric
 from sqlalchemy import DateTime, func, JSON 
 from .category import Category
 
@@ -10,10 +9,10 @@ class Transaction(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     date: dates = Field(description="取引日")
     category: Category  = Relationship(back_populates="transactions")
-    amount: Decimal = Field(description="金額")
+    amount: Numeric = Field(description="金額", sa_column=Column(Numeric(12, 2)))
     memo: Optional[str] = None
     payment_method_pk: str = Field(default=None, foreign_key="paymentmethod.pk", description="支払い方法")
-    repeat: Optional[Dict] = Field(default=None, description="繰り返し設定", sa_column=Column(JSON))
+    repeat: Optional[JSON] = Field(default=None, description="繰り返し設定", sa_column=Column(JSON))
     receipt_url: Optional[str] = Field(default=None, description="レシート画像のURL")
     created_at: Optional[datetime] = Field(default=None, sa_column=Column(DateTime(timezone=True), server_default=func.now()), description="作成日時")
     updated_at: Optional[datetime] = Field(default=None, sa_column=Column(DateTime(timezone=True), onupdate=func.now()), description="更新日時")
